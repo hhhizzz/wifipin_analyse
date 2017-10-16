ca2af383353b5382a9078226a135897e9f747f7a
root <root@host1.com>
2017-10-16 20:03:59 +0800

    改用flume

--- receive-core/wifiPIn_receive.py
+++ receive-core/wifiPIn_receive.py
@@ -1,21 +1,24 @@
 # --coding:utf8--
 from flask import Flask, request
 import json
-from kafka import KafkaProducer
+import requests
 
 app = Flask(__name__)
-producer = KafkaProducer(bootstrap_servers='localhost:6666')
 
-
+url = 'http://host2.com:54321'
+headers = {'content-type': 'application/json'}
 @app.route('/dsky', methods=["POST", "GET"])
 def hello_world():
     data = request.values.get("data")
-    data2 = json.loads(data)
-    beau_data = json.dumps(data2, indent=1, ensure_ascii=False)
-    print(beau_data)
-    producer.send("test", data.encode("utf8"))
+    body = json.dumps(data)
+    payload = [{
+            "body": body
+    }]
+    response = requests.post(
+            url, data=json.dumps(payload), headers=headers)
+    print(body)
     return "Hello"
 
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=6670, debug=True)
+    app.run(host="0.0.0.0", port=8866, debug=True)
