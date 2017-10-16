4721fa1b862afb1ea7388d77802ef23e7f0b1ac0
跑得比谁都快 <qiwei_huang@qq.com>
2017-10-16 16:00:02 +0800

    添加了几个表 准备改用mysql存储数据

--- web-core/models.py
+++ web-core/models.py
@@ -18,29 +18,40 @@ class User(UserMixin, db.Model):
         return check_password_hash(self.password, password)
 
 
+class wifiPin(db.Model):
+    __tablename__ = "wifiPin"
+    id = db.Column(db.String(10), primary_key=True)
+
+
+class UserWithWifi(db.Model):
+    __tablename__ = "userWithWifi"
+    userId = db.Column(db.Integer, db.ForeignKey("User.id"), primary_key=True)
+    wifiId = db.Column(db.String(10), db.ForeignKey("wifiPin.id"), primary_key=False)
+
+
 class UserNumber(db.Model):
     __tabblename__ = "UserNumber"
-    id = db.Column(db.String(10), primary_key=True)
+    id = db.Column(db.String(10), db.ForeignKey("wifiPin.id"), primary_key=True)
     time = db.Column(db.Date, primary_key=True)
     number = db.Column(db.Integer, nullable=False, default=0)
 
 
 class UserRate(db.Model):
     __tabblename__ = "UserRate"
-    id = db.Column(db.String(10), primary_key=True)
+    id = db.Column(db.String(10), db.ForeignKey("wifiPin.id"), primary_key=True)
     time = db.Column(db.Date, primary_key=True)
     rate = db.Column(db.Float, nullable=False, default=0.0)
 
 
 class Stay(db.Model):
     __tabblename__ = "Stay"
-    id = db.Column(db.String(10), primary_key=True)
+    id = db.Column(db.String(10), db.ForeignKey("wifiPin.id"), primary_key=True)
     time = db.Column(db.Date, primary_key=True)
     stay = db.Column(db.Float, nullable=False, default=0.0)
 
 
 class Periodic(db.Model):
     __tabblename__ = "Periodic"
-    id = db.Column(db.String(10), primary_key=True)
+    id = db.Column(db.String(10), db.ForeignKey("wifiPin.id"), primary_key=True)
     time = db.Column(db.Date, primary_key=True)
     space = db.Column(db.Float, nullable=False, default=0.0)
