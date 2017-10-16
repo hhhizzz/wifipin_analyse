207b88165326d3c24253a23c466fe6b12369ae1a
跑得比谁都快 <qiwei_huang@qq.com>
2017-10-16 20:18:00 +0800

    改为多线程

--- receive-core/wifiPIn_receive.py
+++ receive-core/wifiPIn_receive.py
@@ -2,20 +2,27 @@
 from flask import Flask, request
 import json
 import requests
+import _thread
 
 app = Flask(__name__)
 
 url = 'http://host2.com:54321'
 headers = {'content-type': 'application/json'}
+
+
+def send(data):
+    requests.post(
+        url, data=json.dumps(data), headers=headers)
+
+
 @app.route('/dsky', methods=["POST", "GET"])
 def hello_world():
     data = request.values.get("data")
     body = json.dumps(data)
     payload = [{
-            "body": body
+        "body": body
     }]
-    response = requests.post(
-            url, data=json.dumps(payload), headers=headers)
+    _thread.start_new(send, (json.dumps(payload),))
     print(body)
     return "Hello"
 
